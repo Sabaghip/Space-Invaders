3ea6d9be60700c3738f1b603a82869dcfccc76c6
sabaghip <sabaghi.p@gmail.com>
2023-01-28 16:18:41 +0330

    add objects

--- game.h
+++ game.h
@@ -1,3 +1,13 @@
 
-
 char name[100];
+
+char array[20][4][10];
+
+struct player{
+    int x;
+    int y;
+    int health;
+};
+struct player me = {2, 19};
+int enemiesHealths[20][4];
+int shots[20][4];
